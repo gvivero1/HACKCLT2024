96d5bd02356d5464f6f6bc3834c81e9d2efa87a0
wbermel3rd <willathrill@gmail.com>
2024-04-06 02:37:45 -0400

    finished user schema

--- models/user.js
+++ models/user.js
@@ -5,27 +5,38 @@ const Schema = mongoose.Schema;
 const userSchema = new mongoose.Schema({
   email: { type: String },
   password: { type: String },
+  skills: [{type: String}],
+  experienceIds: [{type: String}],
+  blueprintIds: [{type: String}],
+  highestEdu: { type: String },
+  eduGpa: { type: Number },
+});
+
+const blueprintSchema = new mongoose.Schema({
+  blueprintId: {type: String},
+
 });
 
 userSchema.pre('save', function(next) {
-    let user = this;
-    if (!user.isModified('password')) {
-        return next();
-    }
+  let user = this;
+  if (!user.isModified('password')) {
+      return next();
+  }
 
-    bcrypt
-        .hash(user.password, 10)
-        .then((hash) => {
-            user.password = hash;
-            next();
-        })
-        .catch((error) => next(error));
+  bcrypt
+      .hash(user.password, 10)
+      .then((hash) => {
+          user.password = hash;
+          next();
+      })
+      .catch((error) => next(error));
 });
 
 userSchema.methods.checkPassword = function(inputPassword) {
-    let user = this;
-    return bcrypt.compare(inputPassword, user.password);
+  let user = this;
+  return bcrypt.compare(inputPassword, user.password);
 };
 const User = mongoose.model("user", userSchema);
+
 
 module.exports = User;
