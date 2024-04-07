123d7bc5caa62ff28934f4ddbacb3531c44454b0
gvivero1 <gvivero1@uncc.edu>
2024-04-06 22:02:30 -0400

    removed flash errors removed console log

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,12 +17,9 @@ exports.postNew = async (req, res, next) => {
     //code to create a new user account
     const {email, password } = req.body;   
     const user = new User({ email, password });
-    console.log(user);
 
     try {
         await user.save();
-        // Flash Success 
-        req.flash('success', 'Account has been created');
         res.redirect('/users/login');
     } catch (error) {
         res.status(500).json({ error });
